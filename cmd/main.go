76f77145417e8f8438bcd61ec6b0604b0174e9c5
xrodazxx <draturi2535@gmail.com>
2024-10-21 16:12:38 +0200

    ToDo

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/xrodazxx/TODO/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatal("error occured while running http server:%s", err.Error())
+	}
+
+}
