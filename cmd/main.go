21814aef8b898b543fd923f017309f298f5a37ee
xrodazxx <draturi2535@gmail.com>
2024-10-21 17:20:43 +0200

    245tyj

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	apptodo "github.com/xrodazxx/App-ToDO"
+
 	"github.com/xrodazxx/App-ToDO/pkg/handler"
 )
 
 func main() {
 	handlers := new(handler.Handler)
-	srv := new(todo.server)
+	srv := new(apptodo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server:%s", err.Error())
 	}
