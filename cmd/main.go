27733b4054a389c558a36a4eaa6be8670f2a15ca
xrodazxx <draturi2535@gmail.com>
2024-10-22 12:10:48 +0200

    cvbb

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	apptodo "github.com/xrodazxx/App-ToDO"
 
 	"github.com/xrodazxx/App-ToDO/pkg/handler"
@@ -11,13 +12,21 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs:%s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(apptodo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server:%s", err.Error())
 	}
 
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
